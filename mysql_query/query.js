f57e0971f63e456aee1eb6d54a025b0a65bb5e18
Sunil Kharbash <sunil.kharbash@finoit.co.in>
2018-07-13 19:01:44 +0530

    puppies all apis completed

--- mysql_query/query.js
+++ mysql_query/query.js
@@ -20,21 +20,68 @@ connection.query('select * from pups',(err,results,fields)=>{
           });
 
 })
+  }
 
-    // db.any('select * from pups')
-    //   .then(function (data) {
+  function createPuppy(req, res, next) {
+    req.body.age = parseInt(req.body.age);
+    var bodyObj = [req.body.name,req.body.breed,req.body.age,req.body.sex]
+    var sql = 'INSERT INTO pups(`name`,breed,age,sex) VALUES(?,?,?,?)';
+    connection.query(sql,bodyObj,(err,results,fields)=>{
+        if(err) return next(err)
+        res.status(200)
+              .json({
+                status: 'success',
+                message: 'Inserted one puppy'
+              });
+
+    })
+  }
+
+  function updatePuppy(req, res, next) {
+    var sql = "UPDATE pups SET `name`= ?, breed = ?,age = ?, sex = ? WHERE id = ?";
+    var bodyObj = [req.body.name, req.body.breed, parseInt(req.body.age),
+            req.body.sex, parseInt(req.params.id)]
+    connection.query(sql,bodyObj,(err,results,fields)=>{
+        console.log(bodyObj)
+        if(err) return next(err)
+            res.status(200)
+          .json({
+            status: 'success',
+            message: 'Updated puppy'
+          });
+    });
+  }
+
+  function removePuppy(req, res, next) {
+    var pupID = parseInt(req.params.id);
+    var sql = "DELETE FROM pups  WHERE id = ?";
+
+    connection.query(sql,pupID,(err,results,fields)=>{
+        if(err) return (err);
+        res.status(200)
+           .json({
+               status: 'success',
+               message: `Removed ${results.rowCount} puppy`
+           })
+    })
+    
+    // db.result('delete from pups where id = $1', pupID)
+    //   .then(function (result) {
+    //     /* jshint ignore:start */
     //     res.status(200)
     //       .json({
     //         status: 'success',
-    //         data: data,
-    //         message: 'Retrieved ALL puppies'
+    //         message: `Removed ${result.rowCount} puppy`
     //       });
+    //     /* jshint ignore:end */
     //   })
     //   .catch(function (err) {
     //     return next(err);
     //   });
   }
-
   module.exports ={
-      getAllPuppies:getAllPuppies
+      getAllPuppies:getAllPuppies,
+      createPuppy: createPuppy,
+      updatePuppy: updatePuppy,
+      removePuppy: removePuppy
   }
--- routes/index.js
+++ routes/index.js
@@ -56,4 +56,74 @@ var db = require('../mysql_query/query.js');
 
 router.get('/api/puppies',db.getAllPuppies);
 
+/**
+ * @swagger
+ * /api/puppies:
+ *   post:
+ *     tags:
+ *       - Puppies
+ *     description: Creates a new puppy
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: puppy
+ *         description: Puppy object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Puppy'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+router.post('/api/puppies', db.createPuppy);
+
+/**
+ * @swagger
+ * /api/puppies/{id}:
+ *   put:
+ *     tags:
+ *       - Puppies
+ *     description: Updates a single puppy
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Puppy's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: puppy
+ *         description: Puppy object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Puppy'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+router.put('/api/puppies/:id', db.updatePuppy);
+
+/**
+ * @swagger
+ * /api/puppies/{id}:
+ *   delete:
+ *     tags:
+ *       - Puppies
+ *     description: Deletes a single puppy
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Puppy's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
+router.delete('/api/puppies/:id', db.removePuppy);
+
 module.exports = router;
